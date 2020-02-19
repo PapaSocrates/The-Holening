0d176d64e67eb391e88fe343d581a0e993405877
PapaSocrates <raulregadera@gmail.com>
2020-02-19 19:23:55 +0100

    Menu funcional
    El menu tiene gran parte de la funcionalidad, faltan los sprites y la carga de niveles. Posible selector de dificultad (?)

---
+++ Assets/Scripts/cambiaCanvas.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class cambiaCanvas : MonoBehaviour
+{
+    public Canvas cInicio;
+    public Canvas cStart;
+    public Canvas cInstr;
+    public Canvas cTurnoff;
+
+
+    private void Start()
+    {
+        cStart.enabled = false;
+        cInstr.enabled = false;
+        cTurnoff.enabled = false;
+        cInicio.enabled = true;
+    }
+    public void ActivateStart()
+    {
+        cStart.enabled = true;
+        cInstr.enabled = false;
+        cTurnoff.enabled = false;
+        cInicio.enabled = false;
+    }
+
+    public void ActivateInstructions()
+    {
+        cStart.enabled = false;
+        cInstr.enabled = true;
+        cTurnoff.enabled = false;
+        cInicio.enabled = false;
+    }
+
+    public void Turnoff()
+    {
+        cStart.enabled = false;
+        cInstr.enabled = false;
+        cTurnoff.enabled = true;
+        cInicio.enabled = false;
+    }
+
+    public void Inicio()
+    {
+        cStart.enabled = false;
+        cInstr.enabled = false;
+        cTurnoff.enabled = false;
+        cInicio.enabled = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
