9c71a8c6585a16f7bf8f630c046fdbc8d1717454
PapaSocrates <raulregadera@gmail.com>
2020-02-19 18:48:22 +0100

    Primer commit, menu

---
+++ Assets/Scripts/luz_mov.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class luz_mov : MonoBehaviour
+{
+
+    public float targetRotacion;
+    public float rotateSpeed;
+    public bool derecha = true;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float angle = rotateSpeed * Time.deltaTime;
+        print(transform.rotation.eulerAngles.z);
+
+
+        if (transform.rotation.eulerAngles.z >= targetRotacion && transform.rotation.eulerAngles.z < 35 && derecha == true)
+        {
+            rotateSpeed = -rotateSpeed;
+            derecha = false;
+            
+        }
+
+        else if (transform.rotation.eulerAngles.z <= 360 - targetRotacion && transform.rotation.eulerAngles.z > 35 && derecha == false)
+        {
+            print("girar");
+            rotateSpeed = -rotateSpeed;
+            derecha = true;
+
+        }
+
+        transform.rotation *= Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
